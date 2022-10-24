c25d37f55ec18bef7b716c0a3796e3ec9bebdb21
JinSung Hwang <jinsunghwang@HJS.local>
2022-10-24 11:23:16 +0900

    feature: 회원 웹 기능 - 홈 화면 추가
    지식
    1. springboot에서는 url이 입력되면 spring container(controller)를 먼저 살피고 거기에 값이 없으면 html 파일을 살핀다.

---
+++ src/main/java/hello/hellospring/contoller/Home.java
@@ -0,0 +1,15 @@
+package hello.hellospring.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+  @GetMapping("/")
+  public String home() {
+    return "home";
+  }
+
+
+}
