b0443815b1a3cbf6f2e63a9678e80acf9efcaae3
JinSung Hwang <jinsunghwang@HJS.local>
2022-10-24 11:55:57 +0900

    feature: 회원 웹 기능 - 조회

--- src/main/java/hello/hellospring/contoller/MemberController.java
+++ src/main/java/hello/hellospring/contoller/MemberController.java
@@ -2,11 +2,13 @@ package hello.hellospring.contoller;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
-import hello.hellospring.contoller.MemberForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,7 +25,6 @@ public class MemberController {
     return "members/createMemberForm";
   }
 
-
   @PostMapping("/members/new")
   public String create(MemberForm form) {
     Member member = new Member();
@@ -33,5 +34,12 @@ public class MemberController {
     return "redirect:/";
   }
 
+  @GetMapping("/members")
+  public String listMembers(Model model) {
+    List<Member> members = memberService.findMembers();
+
+    model.addAttribute("members", members);
+    return "members/memberList";
+  }
 
 }
